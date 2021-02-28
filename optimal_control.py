9b801c8f75438e61b3455ae762b204dd6cd6a573
abcamiletto <abcamiletto@gmail.com>
2021-02-28 10:48:30 +0100

    better printing

--- optimal_control.py
+++ optimal_control.py
@@ -2,18 +2,18 @@ import casadi as cs
 from urdf2casadi import urdfparser as u2c
 import numpy as np
 import matplotlib.pyplot as plt
+from math import ceil
 
-
+%matplotlib inline
 
 class Urdf2Moon:
     def __init__(self, urdf_path, root, tip):
-        self.load_urdf(urdf_path)
+        self.robot_parser = self.load_urdf(urdf_path)
 
         # Store inputs
         self.root = root
         self.tip = tip
-        self.rk_intervals = 4
-        
+
         # Get basic info
         self.num_joints = self.get_joints_n(self.root, self.tip)
         self.q, self.q_dot, self.u_hat = self.define_symbolic_vars(self.num_joints)
@@ -24,12 +24,13 @@ class Urdf2Moon:
 
 
 
-    def solve(self, cost_func, time_horizon=5, control_steps=100, initial_cond=[1,1,0,0], upper_u = None, lower_u=None):
+    def solve(self, cost_func, time_horizon=5, control_steps=100, initial_cond=[1,1,0,0], upper_u = None, lower_u=None, rk_interval=4):
         #Store Values
         self.cost_func = cost_func
         self.T = time_horizon
         self.N = control_steps
         self.initial_cond = initial_cond
+        self.rk_intervals = rk_interval
         # Fix upper values if not given
         if upper_u == None:
             self.upper_u = [float('inf')] * self.num_joints
@@ -43,17 +44,18 @@ class Urdf2Moon:
         self.F = self.rk4(self.f, self.T, self.N, self.rk_intervals)
         self.u_hat_opt = self.nlp_solver(self.initial_cond, self.upper_x, self.lower_x, self.upper_u, self.lower_u)
         self.x_opt = self.get_x_opt(self.F, self.u_hat_opt, self.initial_cond)
-        
-    
+
+
     def define_symbolic_vars(self, num_joints):
         q = cs.SX.sym("q", num_joints)
         q_dot = cs.SX.sym("q_dot", num_joints)
-        u_hat = cs.SX.sym("u_hat", num_joints) 
+        u_hat = cs.SX.sym("u_hat", num_joints)
         return q, q_dot, u_hat
 
     def load_urdf(self, urdf_path):
-        self.robot_parser = u2c.URDFparser()
-        self.robot_parser.from_file(urdf_path)
+        robot_parser = u2c.URDFparser()
+        robot_parser.from_file(urdf_path)
+        return robot_parser
 
     def get_joints_n(self, root, tip):
         return self.robot_parser.get_n_joints(root, tip) #return the number of actuated joints
@@ -64,12 +66,12 @@ class Urdf2Moon:
 
     def get_motion_equation_matrix(self, root, tip, q, q_dot):
         # load inertia terms (function)
-        M_sym = self.robot_parser.get_inertia_matrix_crba(root_link, end_link)
+        M_sym = self.robot_parser.get_inertia_matrix_crba(root, tip)
         # load gravity terms (function)
         gravity_u2c = [0, 0, -9.81]
-        G_sym = self.robot_parser.get_gravity_rnea(root_link, end_link, gravity_u2c)
+        G_sym = self.robot_parser.get_gravity_rnea(root, tip, gravity_u2c)
         # load Coriolis terms (function)
-        C_sym = self.robot_parser.get_coriolis_rnea(root_link, end_link)
+        C_sym = self.robot_parser.get_coriolis_rnea(root, tip)
         return M_sym(q), C_sym(q, q_dot), G_sym(q)
 
     def get_diff_eq(self, cost_func):
@@ -86,8 +88,8 @@ class Urdf2Moon:
         dt = T/N/m
 
         # variable definition for RK method
-        X0 = cs.MX.sym('X0', 4)
-        U = cs.MX.sym('U', 2)
+        X0 = cs.MX.sym('X0', self.num_joints * 2)
+        U = cs.MX.sym('U', self.num_joints)
 
         # Initial value
         X = X0
@@ -100,8 +102,8 @@ class Urdf2Moon:
             k3, k3_q = f(X + dt/2 * k2, U)
             k4, k4_q = f(X + dt * k3, U)
             # update the state
-            X = X + dt/6*(k1 +2*k2 +2*k3 +k4)    
-            # update the cost function 
+            X = X + dt/6*(k1 +2*k2 +2*k3 +k4)
+            # update the cost function
             Q = Q + dt/6*(k1_q + 2*k2_q + 2*k3_q + k4_q)
 
         F = cs.Function('F', [X0, U], [X, Q],['x0','p'],['xf','qf'])
@@ -112,7 +114,7 @@ class Urdf2Moon:
         u       = []    #input vector
         u_g     = []    #initial guess
         lbu     = []    #lower bounds of inputs
-        ubu     = []    #upper bounds of inputs 
+        ubu     = []    #upper bounds of inputs
         J       = 0     #initial value of cost func
         g       = []    #joint state for all timesteps
         lbx     = []    #lower bounds of states
@@ -127,17 +129,18 @@ class Urdf2Moon:
             ubx += upper_x
 
         Xk = cs.MX(initial_cond) # MUST be coherent with the condition specified abow
+
         # Integration!
         for k in range(self.N):
-            Uk = cs.MX.sym('U_' + str(k), 2) # generate the k-th control command, 2x1 dimension
+            Uk = cs.MX.sym('U_' + str(k), self.num_joints) # generate the k-th control command, 2x1 dimension
             u += [Uk]        # list of commands [U_0, U_1, ..., U_(N-1)]
 
             Fk = self.F(x0=Xk, p=Uk)     #That's the actual integration!
             Xk = Fk['xf']
             J  = J+Fk['qf']
-    
+
             # Add inequality constraint
-            g += [Xk[0], Xk[1]]    # g += [x_1, x_2, x_1d, x_2d]
+            g.append(Xk[0:self.num_joints])    # g += [x_1, x_2, x_1d, x_2d]
 
         # Define the problem to be solved
         problem = {'f': J, 'x': cs.vertcat(*u), 'g': cs.vertcat(*g)}
@@ -158,30 +161,32 @@ class Urdf2Moon:
             u_t = cs.vertcat(u[k*self.num_joints:(k+1)*self.num_joints])
             Fk = F(x0=x_opt[-1], p=u_t)
             x_opt += [Fk['xf'].full()]
-            
-        # now we want x_res = [q1,q2,q1_dot, q2_dot]    
+
+        # now we want x_res = [q1,q2,q1_dot, q2_dot]
         x_res = [np.array([r[k] for r in x_opt], dtype='f') for k in range(2*self.num_joints)]
         return x_res
 
     def print_results(self):
         tgrid = [self.T/self.N*k for k in range(self.N+1)]
 
-        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2)
-        ax1.plot(tgrid, self.x_opt[0], '-')
-        ax1.plot(tgrid, self.x_opt[self.num_joints], '--')
-        ax1.plot(tgrid[1:], self.u_hat_opt[::self.num_joints], '-.')
-        ax1.legend(['q1','q1_dot', 'u1'])
+        fig, axes = plt.subplots(nrows=ceil(self.num_joints/2), ncols=2, figsize=(15, 4*ceil(self.num_joints/2)))
 
-        ax2.plot(tgrid, self.x_opt[1], '-')
-        ax2.plot(tgrid, self.x_opt[self.num_joints+1], '--')
-        ax2.plot(tgrid[1:], self.u_hat_opt[1::self.num_joints], '-.')
-        ax2.legend(['q2','q2_dot', 'u2'])
-        fig.show()
-        
+        for idx, ax in enumerate(fig.axes):
+            self.get_ax(ax, idx, tgrid)
+
+        return fig
+
+    def get_ax(self, ax, idx, tgrid):
+        ax.plot(tgrid, self.x_opt[idx], '-')
+        ax.plot(tgrid, self.x_opt[self.num_joints+idx], '--')
+        ax.plot(tgrid[1:], self.u_hat_opt[idx::self.num_joints], '-.')
+        ax.legend(['q'+str(idx),'q' + str(idx) +'_dot', 'u' + str(idx)])
+        return ax
+
 
 if __name__ == '__main__':
     urdf_path = "../urdf/rrbot.urdf"     # path to urdf
-    root_link = "link1" 
+    root_link = "link1"
     end_link = "link3"
 
     def my_cost_func(q, qd, u):
